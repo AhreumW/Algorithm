9c80b6cb15883c0c3f1d541a87d48643e52bbfa8
TJ <TJ@DESKTOP-J2LI200>
2020-04-10 17:56:07 +0900

    매일프로그래밍

---
+++ algorithmStudy/src/mailprogramming/CoddingTest_0405_2020.java
@@ -0,0 +1,26 @@
+package mailprogramming;
+
+import java.util.Scanner;
+
+public class CoddingTest_0405_2020 {
+
+	public static void main(String[] args) {
+		// 문제
+	/*	정수 배열과 정수 k가 주어지면 모든 원소를 k칸씩 앞으로 옮기시오.
+		
+		input: [1, 2, 3, 4, 5], k=2
+		output: [3, 4, 5, 1, 2]
+
+		input: [0, 1, 2, 3, 4], k=1
+		output: [1, 2, 3, 4, 0]
+
+		시간복잡도를 최대한 최적화 하시오.*/
+		
+		int[] numArr = new int[5];
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+	}
+
+}
