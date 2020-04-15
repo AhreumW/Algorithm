77538b1a78c4e0a0dfd730f41b6edb1d3be135d1
dkfma <dkfma@AhReum>
2020-04-16 00:12:25 +0900

    스터디 04월14일 문제

---
+++ algorithmStudy/src/array/studygroup0414_1.java
@@ -0,0 +1,15 @@
+package array;
+
+public class studygroup0414_1 {
+/*
+	[A][B][C][ ][ ][ ]
+	를 다음과 같이 만들어라 
+	[A][B][C][C][B][A]
+*/
+	//다른 사람들 풀이 
+	//또 다른 배열을 만들거나 
+	//count변수 사용
+	
+	//내 풀이 
+	
+}
---
+++ algorithmStudy/src/array/studygroup0414_4.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.Scanner;
+
+public class studygroup0414_4 {
+/*
+	4번문제
+	사용자로부터 단어 하나를 입력받은뒤,
+	그 문자열 내에 A부터 Z까지의 알파벳
+	이 각각 몇개씩 등장하는지 세어 출력
+	하는 프로그램을 작성하세요. 대소문자
+	를 같이 처리하고, 등장하지 않는 알파
+	벳은 출력하지 않게 합니다. 알파벳
+	이외의 문자는 무시합니다.
+	실행결과
+	단어를 입력하세요 : superCalifragilisticexpialidocious
+*/	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	
+}
